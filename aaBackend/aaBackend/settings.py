de56f0865de9522feeefb68538af197b6aa8eb51
ShijasMkt <shijumkt@gmail.com>
2025-07-08 15:56:16 +0530

    added runtime.txt

--- aaBackend/aaBackend/settings.py
+++ aaBackend/aaBackend/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['your-render-url.onrender.com']
 
 
 # Application definition
